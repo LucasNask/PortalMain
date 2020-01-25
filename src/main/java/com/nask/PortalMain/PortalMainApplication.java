f22da40ff58cd0efe50ace8882e2564d88ff60fd
LucasNask <Lucas_nascimento1@hotmail.com>
2020-01-24 22:53:51 -0300

    Tagawa ta namorando

---
+++ src/main/java/com/nask/PortalMain/PortalMainApplication.java
@@ -0,0 +1,13 @@
+package com.nask.PortalMain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nask/PortalMain/PortalMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nask.PortalMain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
